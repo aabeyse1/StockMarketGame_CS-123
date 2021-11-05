fb09ff144632eb0d23b354b0b6014211bf318ae9
SiliconUnicorn <micahhansonbrook@icloud.com>
2021-11-05 09:37:52 -0500

    Initial code structure
    Entered the initial code structure for the program

---
+++ stockMarket/code/coreCode/main.py
@@ -0,0 +1,16 @@
+'''
+File: main.py
+Project: Stock Market Game
+Date: 5 Nov 2021
+Author(s): Micah Hansonbrook
+'''
+
+def main():
+    '''
+    The main function of the program
+    :return: None
+    '''
+    print("No Action")
+
+if __name__ == "__main__":
+    main()
