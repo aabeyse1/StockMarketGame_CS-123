842d81d3e551c9b7fa34e8500c999cc2fcb2a158
aabeyse1 <93781833+aabeyse1@users.noreply.github.com>
2021-11-06 13:24:05 -0500

    Update stockMarket_main.py

--- stockMarket/code/stockMarket/stockMarket_main.py
+++ stockMarket/code/stockMarket/stockMarket_main.py
@@ -1,6 +1,19 @@
-'''
+"""
 File: stockMarket_main.py
 Project: Stock Market Game
 Date: 5 Nov 2021
 Author(s): Arnika
-'''
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def stock(start_price):
+    mu = 0.004
+    sigma = 0.01
+    np.random.seed(0)
+    returns = np.random.normal(loc=mu, scale=sigma, size=100)
+    price = start_price*(1+returns).cumprod()
+    plt.plot(price)
+    plt.show()
